fe23fc08511f8aa8c954bb4b827ef4c5bf216e47
sharfaraz <sharfarazbaari@gmail.com>
2015-04-02 13:21:38 +0530

    First change

---
+++ TestGit/src/FirstObject.java
@@ -0,0 +1,8 @@
+
+public class FirstObject {
+	
+	public void display() {
+		System.out.println("Printng from object");
+	}
+
+}
--- TestGit/src/PrintScreen.java
+++ TestGit/src/PrintScreen.java
@@ -4,6 +4,8 @@ public class PrintScreen {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Printing on screen");
+		FirstObject obj = new FirstObject();
+		obj.display();
 		
 
 	}
